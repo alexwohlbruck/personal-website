6d123654f70007dc755ce29bf0d8e96c440a877e
Alex Wohlbruck <alexwohlbruck@gmail.com>
2021-07-16 15:11:31 -0400

    Begin homepage design

---
+++ src/components/Navigation.vue
@@ -0,0 +1,46 @@
+<template lang="pug">
+  #nav.row.justify-center
+    router-link.nav-item(
+      v-for='link in links'
+      :key='link.to'
+      :to='link.to'
+    )
+      h6 {{ link.name }}
+</template>
+
+<script lang="ts">
+export default {
+  name: 'navigation',
+  data: () => ({
+    links: [
+      {
+        name: 'Home',
+        to: '#home',
+      },
+      {
+        name: 'About',
+        to: '#about',
+      },
+      {
+        name: 'Work',
+        to: '#work',
+      },
+      {
+        name: 'Contact',
+        to: '#contact',
+      },
+    ],
+  }),
+}
+</script>
+
+<style lang="scss">
+#nav {
+  position: fixed;
+  width: 100%;
+}
+.nav-item {
+  display: inline;
+  margin: 50px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,53 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+<template lang="pug">
+  #home.flex.row.justify-center
+    .section.flex.row.align-center
+      .col
+        h4.text-primary Hi, I'm
+        h1.ma-0 Alex
+        h1.ma-0 Wohlbruck.
+        h4.text-primary I'm a web developer.
+
+      .spacer
+
+      .col
+        a.social-link(href='' v-for='social in socials' :key='social')
+          img(:src='require(`@/assets/svg/${social.name}.svg`)' width='30')
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-};
+  name: 'Home',
+  data: () => ({
+    socials: [
+      {
+        name: 'at',
+        href: 'mailto:alexwohlbruck@gmail.com'
+      },
+      {
+        name: 'linkedin',
+        href: 'https://linkedin.com/in/alexwohlbruck'
+      },
+      {
+        name: 'github',
+        href: 'https://github.com/alexwohlbruck'
+      },
+      {
+        name: 'instagram',
+        href: 'https://instagram.com/alexwohlbruck'
+      },
+    ],
+  }),
+}
 </script>
+
+<style lang="scss">
+.section {
+  height: 100vh;
+  width: 80%;
+  max-width: 1200px;
+}
+
+.social-link {
+  margin: 10px 0;
+}
+</style>
