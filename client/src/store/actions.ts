5bc3e644460cef4d2160e2f6f1d36eb36972484d
Alex Wohlbruck <alexwohlbruck@gmail.com>
2021-11-14 16:16:37 -0500

    Use heroku backend on production mode

--- client/src/store/actions.ts
+++ client/src/store/actions.ts
@@ -1,7 +1,7 @@
-import axios from 'axios'
+import axios from '@/axios'
 
 export const getSpotifyPlaybackState = async ({ commit }) => {
-  const { data } = await axios.get('http://localhost:3000/spotify/playback-state')
+  const { data } = await axios.get('spotify/playback-state')
   commit('SET_SPOTIFY_PLAYBACK_STATE', data)
 }
 
