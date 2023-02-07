700a9caa7f2e57a8dfcc3dbd28f75ae21e0eb3b5
Alex Wohlbruck <alexwohlbruck@gmail.com>
2023-02-06 20:17:09 -0500

    Create hamburger nav menu

---
+++ client/src/components/HamburgerMenu.vue
@@ -0,0 +1,69 @@
+<template lang="pug">
+.hamburger-menu
+  button.icon.trigger-icon.p-a-0
+    img(:src='require(`@/assets/svg/menu.svg`)' width='25' @click='openMenu')
+    
+  Transition(name='fade')
+    .menu.p-a-20.col(v-if='menuOpen')
+      button.icon.p-a-0.m-b-20
+        img(:src='require(`@/assets/svg/close.svg`)' width='25' @click='closeMenu')
+
+      .col.gap-15.align-center.justify-center
+        router-link.nav-item(
+          v-for='(link, i) in links'
+          :key='i'
+          :to='{ name: link.to }'
+          :class='{"text-accent": $router.currentRoute.name === link.to}'
+        )
+          Transition(name='fade' mode='out-in')
+            h5(v-if='menuOpen' @click='closeMenu')
+              a.link(:to='{ name: link.to }') {{ link.name }}
+
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Watch } from 'vue-property-decorator'
+
+@Component
+export default class HamburgerMenu extends Vue {
+
+  @Prop({ type: Array, required: true }) links!: any[]
+
+  menuOpen: boolean = false
+
+  closeMenu() {
+    this.menuOpen = false
+  }
+
+  openMenu() {
+    this.menuOpen = true
+  }
+}
+</script>
+
+<style lang="scss">
+@import '@/styles/variables.scss';
+  
+.hamburger-menu {
+  width: 100px;
+
+  .menu {
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    z-index: 3;
+
+    background-color: $dark;
+    border-right: 2px solid $accent;
+
+    .nav-item.router-link-exact-active a {
+      color: $accent;
+    }
+  }
+}
+
+
+
+</style>
--- client/src/components/Navigation.vue
+++ client/src/components/Navigation.vue
@@ -1,83 +1,102 @@
 <template lang="pug">
 .nav
-  #navbar.row.justify-center
-    router-link.nav-item(
-      v-for='(link, i) in links'
-      :key='i'
-      :to='{ name: link.to }'
-    )
-      enter-transition(:delay='.1 * i + .3' direction='up')
-        h6
-          a.link.p-y-20.p-x-20(href='') {{ link.name }}
+  .left(v-if='mobile')
+    hamburger-menu(:links='links')
+
+  .center(v-else)
+    .navbar.row
+      router-link.nav-item(
+        v-for='(link, i) in links'
+        :key='i'
+        :to='{ name: link.to }'
+      )
+        enter-transition(:delay='.1 * i + .3' direction='up')
+          a.text-h6.link.p-y-20.p-x-20 {{ link.name }}
 </template>
 
 <script>
+import { Vue, Component } from 'vue-property-decorator'
 import EnterTransition from '@/components/transitions/EnterTransition.vue'
+import HamburgerMenu from '@/components/HamburgerMenu.vue'
 
-export default {
-  name: 'navigation',
+import { MOBILE_BREAKPOINT } from '@/globals'
+
+@Component({
   components: {
     EnterTransition,
+    HamburgerMenu,
   },
-  data: () => ({
-    links: [
-      {
-        name: 'Home',
-        to: 'home',
-        class: 'text-accent',
-      },
-      {
-        name: 'About',
-        to: 'about',
-      },
-      {
-        name: 'My work',
-        to: 'work',
-      },
-      {
-        name: 'Contact',
-        to: 'contact',
-      },
-    ],
-  }),
+})
+export default class Navigation extends Vue {
+  mobile = 0
+  links = [
+    {
+      name: 'Home',
+      to: 'home',
+      class: 'text-accent',
+    },
+    {
+      name: 'About',
+      to: 'about',
+    },
+    {
+      name: 'My work',
+      to: 'work',
+    },
+    {
+      name: 'Contact',
+      to: 'contact',
+    },
+  ]
+
+  mounted() {
+    window.addEventListener('resize', this.onResize)
+    this.onResize()
+  }
+
+  onResize() {
+    this.mobile = window.innerWidth <= MOBILE_BREAKPOINT
+  }
 }
 </script>
 
 <style lang="scss">
 @import '@/styles/variables.scss';
 
-#navbar {
-  position: fixed;
-  width: 100%;
-  z-index: 2;
-
-  // &:before {
-  //   content: '';
-  //   position: absolute;
-  //   z-index: -1;
-  //   top: 0;
-  //   left: 0;
-  //   width: 100%;
-  //   height: 300%;
-  //   background-color: $dark;
-  //   -webkit-mask-image: -webkit-gradient(linear, center 35%, center bottom, 
-  //   from(rgba(0,0,0,1)), to(rgba(0,0,0,0)));
-  // }
-}
-.nav-item {
-  display: inline;
-  cursor: pointer;
-
-  &.router-link-exact-active a {
-    color: $accent;
-  }
-  
-  .link {
-    display: inline-block;
+.nav {
+  .left, .center {
+    padding: 20px;
+    position: fixed;
+    top: 0;
+    z-index: 5;
   }
 
-  &:hover .link {
-    color: $white;
+  .left {
+    left: 0;
+  }
+
+  .center {
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  .hamburger-menu {
+    margin-right: auto;
+  }
+
+  .navbar {
+    .nav-item {
+      display: inline;
+      cursor: pointer;
+
+      &.router-link-exact-active a {
+        color: $accent;
+      }
+
+      &:hover .link {
+        color: $white;
+      }
+    }
   }
 }
 
